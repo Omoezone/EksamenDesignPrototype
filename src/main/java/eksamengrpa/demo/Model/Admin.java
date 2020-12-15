c60925862e09504745dd7847047d751c54fc7e73
Mullefyren <57482909+Mullefyren@users.noreply.github.com>
2020-12-15 14:35:04 +0100

    Fixet return knap til brugerside med resultat vist. Fixed box1 css div så den kommer i foreground. Rettet så man kan skrive i login igen

--- src/main/java/eksamengrpa/demo/Model/Admin.java
+++
@@ -1,7 +0,0 @@
-package eksamengrpa.demo.Model;
-
-public class Admin extends SuperBruger{
-    public Admin(String email, String firstName, String lastName) {
-        super(email, firstName, lastName);
-    }
-}
